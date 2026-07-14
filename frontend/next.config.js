439a5586a96420771e1b642d03a366b05f897ef9
jjmaritz-cloud <jjmaritz@gmail.com>
2026-07-14 20:01:41 +1000

    Proxy API through OviCore domain for login cookie

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://ovicore-next-api.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
